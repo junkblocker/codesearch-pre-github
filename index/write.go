9a180ad5ab9506ea63ef7dcb24f08d8e087ff682
Manpreet Singh <junkblocker@yahoo.com>
2014-10-11 18:12:01 -0700

    Update contributors and suitable license texts

--- index/write.go
+++ index/write.go
@@ -1,4 +1,6 @@
 // Copyright 2011 The Go Authors.  All rights reserved.
+// Copyright 2013 Manpreet Singh ( junkblocker@yahoo.com ). All rights reserved.
+//
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
